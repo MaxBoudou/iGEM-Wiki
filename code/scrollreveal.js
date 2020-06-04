c2c2cd5b9f8d2e2d66969b8738c804d59771864b
louis podevin <podevin.louis@hotmail.fr>
2020-06-04 21:55:12 +0200

    modifs

--- code/scrollreveal.js
+++ code/scrollreveal.js
@@ -1,3 +1,5 @@
 const sr = ScrollReveal({reset:true, duration: 1000});
-sr.reveal('#Titre1', {});
-sr.reveal('#BacBouclier', {});
+sr.reveal('#card1', {opacity: 1, scale: 0.9, delay: 0});
+sr.reveal('#card2', {delay: 500, scale: 0.9});
+sr.reveal('#card3', {delay: 600, scale: 0.9});
+sr.reveal('#card4', {delay: 700, scale: 0.9});
