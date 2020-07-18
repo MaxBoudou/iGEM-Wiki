a4cc4134b85eda19743ec246876746efeecec3ff
unknown <podevin.louis@hotmail.fr>
2020-07-19 00:36:24 +0200

    modifs

--- code/js/retourHaut.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function() {
-  var duration = 500;
-  jQuery('#retourHaut').click(function(event) {
-   event.preventDefault();
-   jQuery('html, body').animate({scrollTop: 0}, duration);
-   return false;
-   })
-});
